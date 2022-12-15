e22905a6ba3757ec5a5f3b5ccb464517cf5334a0
kenjoroge <njorogeken57@gmail.com>
2022-12-15 01:31:01 -0800

    Number 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <timme>
+/**
+ * main - print letters in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letters;
+
+	for (letter = 'z'; letter >= 'a' --letter)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
