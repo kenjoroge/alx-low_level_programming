0bf145e1dc4b1f622a14457baf2ae89fad1c9782
kenjoroge <njorogeken57@gmail.com>
2022-12-15 11:03:08 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - prints putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	Return (0)
+}
