2322721c59bcfa0b166edca201620d08d0491b07
kenjoroge <njorogeken57@gmail.com>
2022-12-15 01:00:14 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print numbers from zero to ten.
+ *
+ * Return: Alwasys 0 (Success)
+ */
+int main(void)
+{
+	int = i
+	{
+		for (i = '0'; i <= '10'; i++)
+			printf('%d\n', i)
+	}
+	printf('\n');
+
+	Return (0);
+}
