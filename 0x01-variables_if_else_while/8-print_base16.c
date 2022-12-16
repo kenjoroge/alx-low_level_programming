0c507458dd02f85bd948a381f5aae02b21ff42f3
kenjoroge <njorogeken57@gmail.com>
2022-12-15 22:14:28 -0800

    Number 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num = 0; num <= 9; num++)
 	       putchar(num + '0');
-	for  (character = 'a'; character <= 'f'; Character++)
+	for  (character = 'a'; character <= 'f'; character++)
 	{
 		putchar(character);
 	}
