b78be88c05e8b94d1c45dd3d475c2d3227ffb208
kenjoroge <njorogeken57@gmail.com>
2022-12-15 22:59:32 -0800

    Number 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints possible combinations of three digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 0;
+	int hundreds;
+	int tens;
+	int ones;
+
+	for (num = 0; num < 1000; num++)
+	{
+		hundreds = num / 100;
+		tens = (num / 10) % 10;
+		ones = num % 10;
+
+		If (hundreds < tens && tens < ones)
+		{
+			putchar(hundreds + '0');
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (num < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
