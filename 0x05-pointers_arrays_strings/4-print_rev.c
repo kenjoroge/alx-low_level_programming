d42b9b383eb97fee8cf23f7ef251da531a02c9d3
kenjoroge <njorogeken57@gmail.com>
2022-12-19 13:46:46 -0800

    I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,13 +15,15 @@ void print_rev(char *s)
 		if (s[count] == '\0')
 		{
 			break;
+		}
 		else
 			
 	 		count++;
 	}
-	for (count--  count >= 0; count --)
+	for (count--;  count >= 0; count --)
 	{
 		_putchar(s[count]);
+	}
 
 	_putchar('\n');
 }
