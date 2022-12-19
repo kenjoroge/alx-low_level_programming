c7593896de5d3c34d268ef3a5668f8c8b91785a1
kenjoroge <njorogeken57@gmail.com>
2022-12-19 14:46:44 -0800

    A good engineer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.c"
+/**
+ * rev_string - Reverses a string
+ *
+ * @s: store the argument for s
+ *
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	int count = 0;
+	int var;
+	
+	while (s[count] != '\0')
+	{
+		count++
+	}
+	for (var = count; count > 0; var--)
+	{
+		_putchar(s[count]);
+	}
+
+}
