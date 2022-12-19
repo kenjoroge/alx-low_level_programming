9b6970ff7b1251c68b78f03ab109a1a734a512b6
kenjoroge <njorogeken57@gmail.com>
2022-12-19 00:36:23 -0800

    This report

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int _strlen - returns the length of a string
+ *
+ * @len takes the integer value
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; len <= 10; len++)
+	{
+		_putchar(len);
+	}
+	_putchar('\n')
+}
