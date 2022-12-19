81f9ce504657e9158ca0b2e9ec1fd9fc4f0faab9
kenjoroge <njorogeken57@gmail.com>
2022-12-19 01:27:18 -0800

    I do not fear computers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by a new line
+ *
+ * @str: argument for string 
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	char *str;
+	_putchar(str);
+
+	return (0);
+}
