a5d44e92795e13e51965380f8f3f0b2de0d9394a
kenjoroge <njorogeken57@gmail.com>
2022-12-19 23:32:26 -0800

    Jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - prints the time in 24hours
+ *
+ * @t: prints the time in hours
+ *
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int t;
+	int s;
+
+	for ( t = 0; t < 24; t++)
+	{
+		for (s = 0; s < 60; s++)
+		{
+			_putchar((t / 10) + '0');
+			_putchar((t % 10) + '0');
+			_putchar(':');
+			_putchar((s / 10) + '0');
+			_putchar((s % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+	
