3d383be7159e7eff6ea547a54de0b049b3d6f1bb
kenjoroge <njorogeken57@gmail.com>
2022-12-19 23:55:43 -0800

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints time table starting with zero
+ * r for  row, c for colum and i for digiits
+ *
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int r, c, i;
+	
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
+		{
+			i = (r * c);
+			if (( i / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
