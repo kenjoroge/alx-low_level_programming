acd5a9383696807267cb30307f43e097606f916e
kenjoroge <njorogeken57@gmail.com>
2022-12-19 22:43:45 -0800

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @int: gives the integer value
+ *
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	return (ab * ((ab > 0) - (ab < 0)));
+}
