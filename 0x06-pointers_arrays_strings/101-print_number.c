46ea7c020a2052dbe1fb1c35e5f2ea17acdca658
kenjoroge <njorogeken57@gmail.com>
2022-12-21 12:02:56 -0800

    lastone

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number -prints an integer
+ * @n: prints the integer
+ *
+ * Return: Always 0
+ */
+void print_number(int n)
+{
+	unsigned int k;
+
+	k = n;
+	if (n < 0)
+	{
+		_putchar(45);
+		k = -n;
+	}
+	if (n < 10)
+	{
+		print_number(k / 10);
+	}
+	_putchar((num % 10) + '0');
+}
