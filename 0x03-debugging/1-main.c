415258c557254c3a59546879724eb33087adbb20
kenjoroge <njorogeken57@gmail.com>
2022-12-21 21:39:37 -0800

    Like comment subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Checks for an infinite loop
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	
+	/*while (i < 10)*/
+	/*{*/
+	/*	putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
