075241027f275a82374b1e694b18dbc1be7406a0
kenjoroge <njorogeken57@gmail.com>
2022-12-28 23:41:38 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,9 +4,44 @@
  *
  * @n: takes the input
  *
- * Return: Always 0
+ * Return: Always n times table
  */
 void print_times_table(int n)
 {
-	return (0);
+	int i, j;
+
+	if (n <= 15 || n < 0)
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (j == 0)
+					_putchar(((i * j) % 10) + '0');
+				else if ((i * j) <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((i * j) + '0');
+				}
+				else if ((i * j) <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(((i * j) / 10) + '0');
+					_putchar(((i * j) % 10) + '0');
+				}
+				else
+				{
+					_putchar('.');
+					_putchar(' ');
+					_putchar(((i * j) / 100) + '0');
+					_putchar((((i * j) / 10) % 10) + '0');
+					_putchar(((i * j) % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
 }
