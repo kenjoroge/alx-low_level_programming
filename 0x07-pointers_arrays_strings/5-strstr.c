6450738a59516b4691ea68089383cc398db71ecf
kenjoroge <njorogeken57@gmail.com>
2023-01-03 09:10:33 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strstr - finds first occcurence of the substring needle
+ * in the string haystack
+ * @haystack: pointer to character
+ * @needle: the other pointer
+ *
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (i = 0; haystack[i] == needel[i]; i++)
+	{
+		;
+	if (!(needle[i]))
+		return (haystack);
+	}
+	return (0);
+}
