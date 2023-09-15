2278d1a3d1f3eeff25f0d7996dd494686c5a4319
kenjoroge <njorogeken57@gmail.com>
2023-09-15 01:26:18 -0700

    contain the function that selects the correct function to perform the operation asked by the user. You’re not allowed to declare any other function.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,32 +1,30 @@
 #include "3-calc.h"
-#include <stdlib.h>
-
+#include <stddef.h>
 /**
-*get_op_func - selects the correct function to perform
-*the operation asked by the user. You’re not allowed
-*to declare any other function.
-*@s: operator passed as argument
-*
-*Return: A pointer to the function corresponding to
-*the operator given in parameter
-*/
+ * get_op_func - performs operation asked by theuser
+ * @s: string
+ *
+ * Return: Always 0
+ */
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
+	i = 0;
 
-while (ops[i].op != NULL && *(ops[i].op) != *s)
-i++;
-
-return (ops[i].f);
-
+	while (ops[i].op != NULL && s[1] == '\0')
+	{
+		if (ops[i].op[0] == *s)
+			return ((ops[i].f));
+		i++;
+	}
+	return (NULL);
 }
-
