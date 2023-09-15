46d4ad064d74f5bf71aa8fc83a535a6ca1b2560d
kenjoroge <njorogeken57@gmail.com>
2023-09-15 01:29:37 -0700

    Write a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,45 +2,38 @@
 #include <stdlib.h>
 
 /**
- *main -  program that prints the opcodes of its own main function.
- *@argc: number of arguments passed to the program
- *@argv: array of arguments
+ * main -entry point
+ * @argc: operand
+ * @argv: operand
+ * Description: opcodes in hexadecimal
  *
- *Return: on success, 1 or 2 in case of failure
+ * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
+	int i, j;
 
-    int index, bytes;
-    int (*address)(int, char **) = main;
-    unsigned char opcode;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+	j = atoi(argv[1]);
+	if (j < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    bytes = atoi(argv[1]);
-
-    if (bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
-
-    for (index = 0; index < bytes; index++)
-    {
-        opcode = *(unsigned char *)address;
-        printf("%.2x", opcode);
-
-        if (index == bytes - 1)
-            continue;
-        printf(" ");
-        address++;
-    }
-    printf("\n");
-    return (0);
+	for (i = 0; i < j; i++)
+	{
+		printf("%02hhx", *((char *)main + i));
+		if (i < j - 1)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
 
