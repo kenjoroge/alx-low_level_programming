45dfd00488bab7398f2e9ff056e8fca53d9f0ad3
kenjoroge <njorogeken57@gmail.com>
2023-09-19 02:07:31 -0700

    Free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dog
+ * @d: pointer
+ *
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
+
