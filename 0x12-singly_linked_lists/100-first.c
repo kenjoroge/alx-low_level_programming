dab4c848cea774a97e03941bd33dc286c3394699
kenjoroge <njorogeken57@gmail.com>
2023-09-21 09:09:24 -0700

    Use printf

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * first - print before main
+ *
+ * Return: Always zero
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
